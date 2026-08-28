a97e3047f465480b63f9cbc269918f61dcecbebe
Sourish <sourish383@gmail.com>
2026-08-28 20:07:18 +0200

    Implementing once again Sorter.hpp bcs of Concept architecture . I'm thinking about spliting app_main to seperate file and HardwareContext ( I'll name it HardwareConfiguration) also to seperate file . Also I've decided to move startSorting() task handle responsibility inside class Sorter

--- tests/Nema_test.cpp
+++ tests/Nema_test.cpp
@@ -4,20 +4,27 @@
 #include <chrono>
 #include <iostream>
 
+#define iterations_num 10 
+uint32_t plannerCount { 0 } ;
+uint32_t stepperCount { 0 } ;
+uint32_t taskCount1 { 0 } ;
+uint32_t taskCount2 { 0 } ;
+
+
 class MOCKplanner{
 #define LEFT true 
 public:
     
     static etl::optional< MOCKplanner >  create(){  MOCKplanner tmp{} ; return tmp ; }
     motionBlock_t calculateFrequency(const moveBlock_t &move)   { 
-        static uint32_t count { 0 } ;
-        if ( count == 10 ) return  motionBlock_t{ 0 , 0, 0 } ;
-        count++ ;
+        
+        if ( plannerCount == iterations_num) return  motionBlock_t{ 0 , 0, 0 } ;
+        plannerCount++ ;
         
 
-        std::cout << "Planner: calculateFrequency() \n" ; 
+        std::cout << "Planner: calculateFrequency() num : " << plannerCount << std::endl  ; 
         
-        return motionBlock_t{ ( 10u + count )   , ( 10u+ ( count * count))  ,1 } ;
+        return motionBlock_t{ ( 10u + plannerCount )   , ( 10u+ ( plannerCount * plannerCount))  ,1 } ;
     } 
     void stop(){ std::cout << " Planner: stop() \n "; }
     void start(){ std::cout << "Planner: start() \n"; }
@@ -37,7 +44,10 @@ class MOCKstepper {
 public:
     static  etl::optional< MOCKstepper >  create( uint16_t step , uint16_t dir ){  MOCKstepper tmp{}; return tmp;}
 
-    void enqueue( motionBlock_t  motion){  std::cout << "STEPPER: Enqueueing motion \n Engine is running in background ... \n"; }  
+    void enqueue( motionBlock_t  motion){ 
+        
+         stepperCount ++ ; 
+         std::cout << "STEPPER: enqueue() num : " << stepperCount << std::endl ; }  
     
     void stop(bool instant = false  ) { std::cout << "Stepper: stop() \n" ;} 
     void start(){ std::cout << "Stepper : start() \n" ; } 
@@ -64,11 +74,25 @@ public:
     }
 
     void notify( uint8_t message ){  mess = message ; }
-    bool requestStop()  { auto count { 0 }  ; count++ ;  if ( count >= 10 ) return true; stop = false ;}
+    bool requestStop()  { 
+        
+        if ( taskCount1>= 10 ) return true; 
+        taskCount1++ ;  
+        std::cout << "Task:: requestStop() num : " << taskCount1<< std::endl ;
+        stop = false ;
+    }
     bool join(){  while ( running){} ; return true;  }
 
-    static bool stopRequested(){ return stop ; }
+    static bool stopRequested(){   
+        if ( taskCount2>= 10 ) return true ; 
+        std::cout << "Task stopRequested() num : "  << taskCount2 << std::endl ;
+        taskCount2 ++  ; 
+        return false  ; 
+    }
     static bool notifyWait( uint8_t message , uint32_t delay ){  
+        static auto count { 0 } ; 
+        if ( count >= 1 ) return false ;
+        count ++ ; 
         std::cout << "MOCKtask : notifyWait()  " << message << std::endl ;
         return true ;
     } 
@@ -81,29 +105,35 @@ public:
 int main(){
     #define stepPin 0 
     #define dirPin 1 
-    auto  planner = *MOCKplanner::create() ; 
-    auto stepper = *MOCKstepper::create( stepPin , dirPin  ) ;
+    auto  planner { *MOCKplanner::create() } ; 
+    auto stepper { *MOCKstepper::create( stepPin , dirPin  ) };
     etl::optional<MOCKtask> taskspace;  
 
     using NemaMOCK = Nema< MOCKplanner , MOCKstepper , MOCKtask >;
-    auto nemaEng = *NemaMOCK::create(stepPin , dirPin ,planner , stepper , taskspace  );
+    auto nemaEng { *NemaMOCK::create(stepPin , dirPin ,planner , stepper , taskspace  ) };
     
     moveBlock_t testMove1 { 50 } ;
     moveInfo_t notQueInf { false , false } ;
     moveInfo_t QueInf { false , true } ;
 
-    // making move directly (without queue) 
+    nemaEng.start() ; 
+    std::cout << "-----  making move directly (without queue)  ----- " << std::endl ;
     nemaEng.moveToCup( testMove1 ,  notQueInf ) ; // should do 10 times 
 
+    plannerCount  =  0  ;
+    stepperCount = 0  ;
+    taskCount1 = 0  ;
+    taskCount2 = 0  ;
 
 
-    // making move via queue ( spoofing dataRelayTask) 
-    // scenario where Planners queue is full and it is ready to be transfered via dataRelayTask ( we'll be simulationg it ) 
+
+    std::cout << std::endl <<  " making move via queue ( spoofing dataRelayTask)  " << std::endl ;
+    //scenario where Planners queue is full and it is ready to be transfered via dataRelayTask ( we'll be simulationg it ) 
 
     nemaEng.moveToCup( testMove1 , QueInf) ;
     
     auto task = *taskspace ;
 
-    task.taskFunc( task.arg) ;  // dataRelayTask 
+    task.taskFunc( task.arg) ;  // dataRelayTask simulating
 
 }
